d82180d52d6b9e7dd6c6fb6344cc77222f5beb7f
NastyaSinkosvskaya <92142826+NastyaSinkosvskaya@users.noreply.github.com>
2021-12-10 12:41:23 +0300

    Add files via upload

---
+++ samrab.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int n;
+    bool check = true;
+    cout << "n: "; cin >> n;
+    int* array = new int[n];
+    for (int i = 0; i < n; i++) {
+        cout << "[" << i + 1 << "]: ";
+        cin >> array[i];
+    }
+
+    if (array[0] > array[1] && n >= 3)
+    {
+        for (int i = 0; i < n - 2; i += 2) {
+            if (array[i] <= array[i + 1] || array[i + 1] >= array[i + 2]) {
+                check = false;
+                break;
+            }
+        }
+    }
+    else if (array[0] < array[1] && n >= 3)
+    {
+        for (int i = 0; i < n - 2; i += 2)
+        {
+            if (array[i] >= array[i + 1] || array[i + 1] <= array[i + 2])
+            {
+                check = false;
+                break;
+            }
+        }
+    }
+    else check = false;
+    if (!check) cout << "Последовательность не пиловидная";
+
+    else cout << "Последовательность пиловидная";
+}
